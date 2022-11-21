a5331cebe92c627a7b2891fe18d9639304e003fa
Pooja-Manvani <poojamanvani123@gmail.com>
2022-11-21 19:46:57 +0530

    feat: folder-structure

---
+++ src/Components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../assets/login.css'
+
+export default function Login() {
+  return (
+    <div className='wrapper'>
+      <div >layout
+        <div>
+          dbvk
+        </div>
+      </div>
+    </div>
+  )
+}
